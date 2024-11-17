b8be01633942d7e41fc87a29d6d1ff54aa0c41ae
Rajesh Sola <rsola@gitam.edu>
2024-11-17 22:45:05 +0530

    Updated UDP examples

--- socket-prog/udp/udp-rcvr.py
+++ socket-prog/udp/udp-rcvr.py
@@ -3,12 +3,14 @@ UDP Receiver
 """
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 5050
 MESSAGE = b"Hello, World!"
 MAX_SIZE = 1024
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.bind((SERVER_IP, SERVER_PORT))
+#sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 data, addr = sock.recvfrom(MAX_SIZE)
-print("received back message: {1}", data)
+print("client says: {1}", data)
+sock.sendto(data, addr)
--- socket-prog/udp/udp-sender.py
+++ socket-prog/udp/udp-sender.py
@@ -1,8 +1,9 @@
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5050
 MESSAGE = b"Hello, World!"
+MAX_SIZE = 1024
 
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
@@ -10,4 +11,6 @@ print("message: %s" % MESSAGE)
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+data, addr = sock.recvfrom(MAX_SIZE)
+print("server says: {1}", data)
 
